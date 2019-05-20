13d06db299aa03e7fc351d31ddbafdb299c06001
Kazuki Matsumoto <1132081+karasusan@users.noreply.github.com>
2019-05-20 23:45:12 +0900

    Update ReplaceText.ps1

--- ReplaceText.ps1
+++ ReplaceText.ps1
@@ -1,1 +1,1 @@
-(gc $Args[0]) -replace $Args[1], $Args[2] | Out-String | % { [Text.Encoding]::UTF8.GetBytes($_) } | Set-Content -Path $Args[0] -Encoding Byte
+(gc $Args[0]).Replace($Args[1], $Args[2]) | Out-String | % { [Text.Encoding]::UTF8.GetBytes($_) } | Set-Content -Path $Args[0] -Encoding Byte
